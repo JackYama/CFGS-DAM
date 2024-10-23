4d6e52fec37b03767fa9e2a3c348a2d4177c0f76
JackYama <Jackllama2006@gmail.com>
2024-10-23 14:27:04 +0200

    23/10v.7

--- Programación/EjerciciosClase/Bucles/10bucles.c
+++ Programación/EjerciciosClase/Bucles/10bucles.c
@@ -4,7 +4,7 @@ int main(){
 	int num;
 	printf("Introduce un numero:");
 	scanf("%d",&num);
-	for(int fila =1; fila<=num;fila++){
+	for(int fila =1; fila<=num;fila++){//no rellenaentero el programa ya que aqui indicamos que solo queremos que llene hasta el numero de fila que le indicamos abajo con col ya que "col<=fila", esto lo que hace es que no se rellene simplemente coincidan la fila con las columna, esto lo que hace es que no se rellene simplemente coincidan la fila con las columnas 
 		for(int col=1;col<=fila;col++){
 			printf("%d",col);
 			}
